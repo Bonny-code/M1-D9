66dbd3b4f1359b0aa6d1fc7f73a607c31f429eec
Bonny-code <ndiboniface@outlook.com>
2021-07-01 17:40:26 +0100

    btn created

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ for (let number=0; number <= 76; number++) {
 }
 
 }
+
+let button = document.getElementsByClassName("btn")
+let myArray = ["1", "2", "3", "4", "5"]
+
+button.onclick = function(){
+    
+    alert(myarray[Math.floor(Math.random() * myarray.length)]);
+}
